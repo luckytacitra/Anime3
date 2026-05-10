6a94e43b79af95a36f61234242021150ab354192
luckytacitra <6003251012@student.its.ac.id>
2026-05-10 19:08:15 +0700

    anime3.py

--- anime3.py
+++ anime3.py
@@ -40,7 +40,7 @@ if not os.path.exists("users-details-2023.csv"):
 def load_data():
 
     df_anime = pd.read_csv(
-        'anime-dataset-2023.csv'
+        'anime-dataset-2023-clean.csv'
     )
 
     df_user = pd.read_csv(
